812c60c416b59e0cb1087fd616ef1e08bbbe0fd7
bite <mlisbit@gmail.com>
2014-02-02 15:56:03 -0500

    in comes the django

---
+++ lisio/views.py
@@ -0,0 +1,11 @@
+from django.http import HttpResponse
+from django.template import Context
+from django.template.loader import get_template
+from django.shortcuts import render_to_response 
+from django.views.generic.base import TemplateView
+from django.core.context_processors import csrf
+from django.template import RequestContext
+
+def home(request):
+	args= {}
+	return render_to_response('index.html', args, context_instance=RequestContext(request))
